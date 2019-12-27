de2b60072bf81e7f37015ee7a30ab106c9c91e24
edison-cy-yang <edison.cy.yang@gmail.com>
2019-12-27 02:23:46 +0000

    basic setup to embed and play youtube video

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+app.use(express.static("views"));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
 app.listen(PORT, () => {
   console.log( `server listening at ${PORT}`);
 });
